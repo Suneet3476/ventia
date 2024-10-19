b990f2a0c59c98b6fe2c6a1eb3d4fd5e1477c051
Suneet3476 <49185@ismoman.com>
2024-10-19 23:35:11 +0400

    Update profile.js

--- public/profile.js
+++ public/profile.js
@@ -7,3 +7,14 @@ document.getElementById('security-form').addEventListener('submit', function(e) 
   e.preventDefault();
   alert('Password Updated Successfully!');
 });
+
+
+document.getElementById('signout-button').addEventListener('click', function () {
+  // Redirect to a blank page with a "Successfully signed out" message
+  document.body.innerHTML = "<div style='color: #fff; text-align: center; font-size: 2rem; margin-top: 50px;'>Successfully signed out</div>";
+  
+  // After 3 seconds, redirect to login.html
+  setTimeout(function () {
+    window.location.href = 'login.html';
+  }, 3000); // 3 second delay
+});
