862b647db7973b7cc76d25d373f65c58ad57d21f
Suneet3476 <49185@ismoman.com>
2024-10-19 17:23:53 +0400

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,25 @@
+const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(path.join(__dirname, '../public'))); // Serve static files
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/index.html')); // Serve the homepage
+// Connect to MongoDB
+mongoose.connect('mongodb+srv://<username>:<password>@cluster0.mongodb.net/ventia-db?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => {
+  console.log('Connected to MongoDB');
+}).catch(err => {
+  console.error('Error connecting to MongoDB', err);
 });
 
-// Other routes can be defined here
+app.use(express.json()); // Parse JSON bodies
+
+// Serve static files (frontend)
+app.use(express.static(path.join(__dirname, '../public')));
+
+// Your backend routes will go here
+
 app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+  console.log('Server running on port 3000');
 });
