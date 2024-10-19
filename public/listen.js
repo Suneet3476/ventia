e0c6a907ac74bdb02eda2ce50f97492725bc5022
Suneet3476 <49185@ismoman.com>
2024-10-19 23:50:37 +0400

    Update listen.js

--- public/listen.js
+++ public/listen.js
@@ -1,1 +1,9 @@
+// listen.js
+document.addEventListener("DOMContentLoaded", () => {
+  const notifyButton = document.querySelector(".notify-button");
+
+  notifyButton.addEventListener("click", () => {
+    alert("You'll be notified when this service is live!");
+  });
+});
 
