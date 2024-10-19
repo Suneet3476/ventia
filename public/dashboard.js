f7a2ff7814f535b4ac2a3c01c6c4b30d23d20004
Suneet3476 <49185@ismoman.com>
2024-10-19 18:46:26 +0400

    Create dashboard.js

---
+++ public/dashboard.js
@@ -0,0 +1,15 @@
+document.getElementById('vent-button').addEventListener('click', () => {
+  alert('Venting session started!');
+});
+
+document.getElementById('listen-button').addEventListener('click', () => {
+  alert('Listening session started!');
+});
+
+document.getElementById('mood-checkin').addEventListener('click', () => {
+  alert('Time to check in on your mood!');
+});
+
+document.getElementById('start-journal').addEventListener('click', () => {
+  alert('Let\'s start journaling!');
+});
