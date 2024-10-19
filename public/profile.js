88632b13d770f63d16087891a6f0f8c68c8a484b
Suneet3476 <49185@ismoman.com>
2024-10-19 22:53:11 +0400

    Create profile.js

---
+++ public/profile.js
@@ -0,0 +1,9 @@
+document.getElementById('profile-form').addEventListener('submit', function(e) {
+  e.preventDefault();
+  alert('Profile Updated Successfully!');
+});
+
+document.getElementById('security-form').addEventListener('submit', function(e) {
+  e.preventDefault();
+  alert('Password Updated Successfully!');
+});
