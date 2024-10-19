7a2bae858a86d9e48c111c96793ad28bc769a326
Suneet3476 <49185@ismoman.com>
2024-10-19 23:55:24 +0400

    Update listen.js

--- public/listen.js
+++ public/listen.js
@@ -1,22 +1,8 @@
+// listen.js
 document.addEventListener("DOMContentLoaded", () => {
   const notifyButton = document.querySelector(".notify-button");
 
   notifyButton.addEventListener("click", () => {
-    showPopup("You’ll be notified when this service is live!");
+    alert("You'll be notified when this service is live!");
   });
-
-  function showPopup(message) {
-    // Create the popup container
-    const popup = document.createElement("div");
-    popup.classList.add("popup");
-    popup.textContent = message;
-
-    // Add the popup to the body
-    document.body.appendChild(popup);
-
-    // Remove the popup after 3 seconds
-    setTimeout(() => {
-      popup.remove();
-    }, 3000);
-  }
 });
